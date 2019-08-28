e6b7019b4b943d166fa91a713c16797295e6f996
eugeneblog <714284150@qq.com>
2019-08-28 17:28:20 +0800

    init

---
+++ src/format/index.js
@@ -0,0 +1,27 @@
+class Format {
+  formatDate(num, format) {
+    // 格式化时间，变成00的形式，不足十位添加0
+    function formatNumber(n) {
+      n = n.toString();
+      return n[1] ? n : "0" + n;
+    }
+
+    var time = new Date(num);
+    var nowObj = {}; // 预先将YMDsmh对应的时间数据存储起来, 然后将Y-M-D字符串里面的YMD替换成对象里面的参数
+
+    nowObj.Y = time.getFullYear();
+    nowObj.M = time.getMonth() + 1;
+    nowObj.D = time.getDate();
+    nowObj.s = formatNumber(time.getHours());
+    nowObj.m = formatNumber(time.getMinutes());
+    nowObj.h = formatNumber(time.getSeconds());
+
+    for (var key in nowObj) {
+      // replace替换 Y-M-D
+      format = format.replace(key, nowObj[key]);
+    }
+    return format;
+  }
+}
+
+module.exports = Format
--- src/index.js
+++ src/index.js
@@ -1,29 +1,22 @@
-(function() {
-  function formatDate(num, format) {
-    // 格式化时间，变成00的形式，不足十位添加0
-    function formatNumber(n) {
-      n = n.toString();
-      return n[1] ? n : "0" + n;
-    }
-    
-    let time = new Date(num);
-    let nowObj = {};
+import Format from './format/index'
 
-    // 预先将YMDsmh对应的时间数据存储起来, 然后将Y-M-D字符串里面的YMD替换成对象里面的参数
-    nowObj.Y = time.getFullYear();
-    nowObj.M = time.getMonth() + 1;
-    nowObj.D = time.getDate();
+// 定义所有函数
+const UtilFn = {}
 
-    nowObj.s = formatNumber(time.getHours());
-    nowObj.m = formatNumber(time.getMinutes());
-    nowObj.h = formatNumber(time.getSeconds());
+Object.assign(UtilFn.prototype, {
+  toString() {
 
-    for (let key in nowObj) {
-      // replace替换 Y-M-D
-      format = format.replace(key, nowObj[key]);
-    }
-    return format;
   }
+})
 
-  formatDate(Date.now(), "Y-M-D");
-})();
+// 对外输出EUtils实例
+class EUtils{
+  constructor(Fn) {
+    // 定义所有工具函数
+    this.Fn = Fn
+  }
+}
+
+const utils = new EUtils(UtilFn)
+
+module.exports = utils
